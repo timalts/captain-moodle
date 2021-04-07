4f2890d86290cae79bf445bbdc4b87c5681eedfc
Du Gardin Emeric <emeric.du-gardin@epita.fr>
2021-04-07 15:52:46 +0100

    Project base
    Signed-off-by: Du Gardin Emeric <emeric.du-gardin@epita.fr>

---
+++ CaptainMoodle/Controllers/FacultyController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CaptainMoodle.Data;
+using Models.Account;
+
+namespace CaptainMoodle.Controllers
+{
+    public class FacultyController : Controller
+    {
+        private readonly CaptainMoodleContext _context;
+
+        public FacultyController(CaptainMoodleContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Faculty
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Faculty.ToListAsync());
+        }
+
+        // GET: Faculty/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var faculty = await _context.Faculty
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (faculty == null)
+            {
+                return NotFound();
+            }
+
+            return View(faculty);
+        }
+
+        // GET: Faculty/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Faculty/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Firstname,Lastname,Id,Username,Password")] Faculty faculty)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(faculty);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(faculty);
+        }
+
+        // GET: Faculty/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var faculty = await _context.Faculty.FindAsync(id);
+            if (faculty == null)
+            {
+                return NotFound();
+            }
+            return View(faculty);
+        }
+
+        // POST: Faculty/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Firstname,Lastname,Id,Username,Password")] Faculty faculty)
+        {
+            if (id != faculty.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(faculty);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FacultyExists(faculty.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(faculty);
+        }
+
+        // GET: Faculty/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var faculty = await _context.Faculty
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (faculty == null)
+            {
+                return NotFound();
+            }
+
+            return View(faculty);
+        }
+
+        // POST: Faculty/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var faculty = await _context.Faculty.FindAsync(id);
+            _context.Faculty.Remove(faculty);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FacultyExists(int id)
+        {
+            return _context.Faculty.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ CaptainMoodle/Data/CaptainMoodleContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Models.Account;
+
+namespace CaptainMoodle.Data
+{
+    public class CaptainMoodleContext : DbContext
+    {
+        public CaptainMoodleContext(DbContextOptions<CaptainMoodleContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Admin> Admin { get; set; }
+
+        public DbSet<Faculty> Faculty { get; set; }
+    }
+}
