28640cb37540150db928ab634e2bf73f5f163e8e
Du Gardin Emeric <emeric.du-gardin@epita.fr>
2021-04-12 17:07:52 +0100

    Update both, student still not finish

--- CaptainMoodle/Models/Account/Student.cs
+++ CaptainMoodle/Models/Account/Student.cs
@@ -3,9 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CaptainMoodle.Models.Account
+namespace Models.Account
 {
     public class Student
     {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public int Birth { get; set; }
+
+        //Waiting for implementation
+        //public List<Course> Course {get; set}
+        public string Campus { get; set; }
+
+        public string Contact { get; set; }
+
+        //Waiting for implementation
+        //public List<Payment> Payment {get; set}
+
+        //Waiting for implementation
+        //public List<Exam> Exam {get; set}
+
+        //Waiting for implementation
+        //public AssignementBook AssignementBook {get; set}
     }
 }
