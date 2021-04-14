5e7dd7233dabc2cce0a7c0ab2d07a4f01a27b941
Du Gardin Emeric <emeric.du-gardin@epita.fr>
2021-04-14 15:56:17 +0100

    Completed Student.cs

--- CaptainMoodle/Models/Account/Student.cs
+++ CaptainMoodle/Models/Account/Student.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models.Object;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,19 +16,18 @@ namespace Models.Account
 
         public int Birth { get; set; }
 
-        //Waiting for implementation
-        //public List<Course> Course {get; set}
+        public List<Course> Course { get; set; }
         public string Campus { get; set; }
 
         public string Contact { get; set; }
 
-        //Waiting for implementation
-        //public List<Payment> Payment {get; set}
+        
+        public List<Payment> Payment { get; set; }
 
-        //Waiting for implementation
-        //public List<Exam> Exam {get; set}
+        
+        public List<Exam> Exam { get; set; }
 
-        //Waiting for implementation
-        //public AssignementBook AssignementBook {get; set}
+        
+        public AssignementBook AssignementBook { get; set; }
     }
 }
