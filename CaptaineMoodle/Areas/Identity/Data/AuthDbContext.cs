2a529061fd6f69d5a37e78cb1732c7b47ed39f97
Du Gardin Emeric <emeric.du-gardin@epita.fr>
2021-04-19 18:02:23 +0100

    base authentication and role

---
+++ CaptaineMoodle/Areas/Identity/Data/AuthDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CaptaineMoodle.Areas.Identity.Data;
+using CaptaineMoodle.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaptaineMoodle.Data
+{
+    public class AuthDbContext : IdentityDbContext<User>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options)
+            : base(options)
+        {
+
+            
+
+        }
+        public DbSet<Payment> Payment { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+
+    
+}
---
+++ CaptaineMoodle/Models/EmailSender.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaptaineMoodle.Models
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            throw new NotImplementedException("No email provider is implemented by default, please Google on how to add one, like SendGrid.");
+        }
+    }
+}
