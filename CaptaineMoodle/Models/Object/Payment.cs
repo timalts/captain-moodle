5566447ad68d7f34698026443aed74fc75a55a05
Timothé Noël <timothe.noel67@gmail.com>
2021-04-20 18:31:11 +0100

    Revert "Add one-to-one relation with User to the Payment model, controller and view"
    This reverts commit d139dd92a80dc356b6a626d2b25a80b70fbfb299.

--- CaptaineMoodle/Models/Object/Payment.cs
+++ CaptaineMoodle/Models/Object/Payment.cs
@@ -8,7 +8,6 @@ namespace CaptaineMoodle.Models
     public class Payment
     {
         public int Id { get; set; }
-        public User User { get; set; }
         public int Amount { get; set; }
         public string Semester { get; set; }
         public bool Paid { get; set; }
