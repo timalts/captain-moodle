29adbf96b4ca711c44d2c2189bb5d83edd485f83
Timothé Noël <timothe.noel67@gmail.com>
2021-04-21 07:07:19 +0100

    Add check to see if user is logged in or not

---
+++ CaptaineMoodle/ViewModels/PaymentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaptaineMoodle.ViewModels
+{
+    public class PaymentViewModel
+    {
+        public PaymentViewModel()
+        {
+            Users = new List<string>();
+        }
+        public int Id { get; set; }
+        public List<String> Users { get; set; }
+        public int Amount { get; set; }
+        public string Semester { get; set; }
+        public bool Paid { get; set; }
+    }
+}
