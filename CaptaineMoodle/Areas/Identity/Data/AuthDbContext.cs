8fa0349c0de65971e7314f912a7ac4031745afc5
Du Gardin Emeric <emeric.du-gardin@epita.fr>
2021-04-23 15:33:25 +0100

    Clean trash file

--- CaptaineMoodle/Areas/Identity/Data/AuthDbContext.cs
+++ CaptaineMoodle/Areas/Identity/Data/AuthDbContext.cs
@@ -21,12 +21,17 @@ namespace CaptaineMoodle.Data
         }
         public DbSet<Payment> Payment { get; set; }
 
+        public DbSet<Course> Course { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
+
+            //Seeding a  'Administrator' role to AspNetRoles table
+            builder.Entity<IdentityRole>().HasData(new IdentityRole { Id = "2c5e174e-3b0e-446f-86af-483d56fd7210", Name = "Admin", NormalizedName = "ADMIN".ToUpper() });
         }
     }
 
