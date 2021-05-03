0a45abe19e901b58ff33c655a7f1f3f1dcd868f8
Emeric <emericdugardin@gmail.com>
2021-05-03 19:36:50 +0100

    Add whole timetable
    Implement the whole timetable

---
+++ CaptaineMoodle/Controllers/TimetableController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaptaineMoodle.Controllers
+{
+    public class TimetableController : Controller
+    {
+        // GET: TimetableController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ CaptaineMoodle/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaptaineMoodle.Models
+{
+    public class Event
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string Text { get; set; }
+        public string Color { get; set; }
+    }
+}
